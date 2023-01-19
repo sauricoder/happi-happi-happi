f278411282eba762eee23fbc68e12719b963c297
sauricoder <122543575+sauricoder@users.noreply.github.com>
2023-01-19 09:55:48 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,44 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
-    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Forever)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . # . # .
+        . . # . .
+        # . . . #
+        . . . . .
+        . # # # .
+        `)
+    music.playMelody("E G E G B C F B ", 120)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Fabulous)
-    music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
+    music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Forever)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
-    music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
+    music.startMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Forever)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . . . . .
+        . # . # .
+        # . # # #
+        `)
+    music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Forever)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . .
+        # # # # #
+        `)
+    music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Forever)
 })
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
